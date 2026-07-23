6e51adfc1c7c15899554517e752ebf38b8e76e1b
SanjanaSPuranik <sanjanapuranik17@gmail.com>
2026-07-23 21:22:04 +0530

    Fix import statement for Booking entity

--- src/main/java/com/san/repository/BookingRepository.java
+++ src/main/java/com/san/repository/BookingRepository.java
@@ -1,6 +1,6 @@
 package com.san.repository;
 
-import com.sak.entity.Booking;
+import com.san.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
